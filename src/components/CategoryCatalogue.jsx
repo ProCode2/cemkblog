5139429b4a521aedaad702c2b63447138ade0c84
ProCode2 <iamprobablycoding@gmail.com>
2022-09-13 15:12:35 +0530

    fix: build errors fixed

--- src/components/CategoryCatalogue.jsx
+++ src/components/CategoryCatalogue.jsx
@@ -5,7 +5,10 @@ const CategoryCatalogue = ({ tags }) => {
     <div className="p-4 my-4">
       <div className="flex justify-center items-center overflow-y-auto space-x-4 max-w-6xl mx-auto">
         {tags?.map((t) => (
-          <span className="py-2 px-4 border border-gray-600 dark:border-gray-100 rounded-md shadow-md dark:shadow-gray-300 bg-slate-300 dark:bg-slate-900 text-sm">
+          <span
+            key={t}
+            className="py-2 px-4 border border-gray-600 dark:border-gray-100 rounded-md shadow-md dark:shadow-gray-300 bg-slate-300 dark:bg-slate-900 text-sm"
+          >
             {t}
           </span>
         ))}
