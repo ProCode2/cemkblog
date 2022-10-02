ec16f0d5e8e392a159a333eb3b3d460c4abd9f21
ProCode2 <iamprobablycoding@gmail.com>
2022-10-03 03:09:52 +0530

    fix: build errors fixed

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,17 +1,15 @@
+import { useRouter } from "next/router";
 import React from "react";
 import ArticleItem from "./ArticleItem";
 
 const ArticleList = ({ posts }) => {
-  let profilePage = false;
-  if (window != undefined) {
-    profilePage = window?.location.href.includes("/profile");
-  }
+  const { asPath } = useRouter();
   return (
     <div className="w-full h-full p-4 flex justify-start items-center mx-auto">
       <div className="max-w-6xl w-full">
         <div className="w-full text-left">
           <p className="text-2xl uppercase mx-auto max-w-6xl w-full">
-            {profilePage ? "Posts by this author" : "Explore"}
+            {asPath ? "Posts by this author" : "Explore"}
           </p>
           <div className="flex flex-col justify-center items-start space-y-4 mt-6">
             {posts?.map((post) => (
