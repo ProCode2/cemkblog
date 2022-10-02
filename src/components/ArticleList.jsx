91dc0d98a3216ce81e8b171c890d3eb785249901
ProCode2 <iamprobablycoding@gmail.com>
2022-10-03 03:02:58 +0530

    chore: styling changes

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -6,7 +6,11 @@ const ArticleList = ({ posts }) => {
     <div className="w-full h-full p-4 flex justify-start items-center mx-auto">
       <div className="max-w-6xl w-full">
         <div className="w-full text-left">
-          <p className="text-2xl uppercase mx-auto max-w-6xl w-full">Explore</p>
+          <p className="text-2xl uppercase mx-auto max-w-6xl w-full">
+            {window?.location.href.includes("/profile")
+              ? "Posts by this author"
+              : "Explore"}
+          </p>
           <div className="flex flex-col justify-center items-start space-y-4 mt-6">
             {posts?.map((post) => (
               <ArticleItem key={post.id} post={post} />
