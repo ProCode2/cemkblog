6bfaa7ebb856784d0774ccec72f2d3995f9e5bbc
Pradipta Ghoshal <50803990+ProCode2@users.noreply.github.com>
2022-10-03 03:11:03 +0530

    chore: styling changes (#1)
    * chore: styling changes
    
    * fix: build errors fixed
    
    * fix: build errors fixed

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,12 +1,16 @@
+import { useRouter } from "next/router";
 import React from "react";
 import ArticleItem from "./ArticleItem";
 
 const ArticleList = ({ posts }) => {
+  const { asPath } = useRouter();
   return (
     <div className="w-full h-full p-4 flex justify-start items-center mx-auto">
       <div className="max-w-6xl w-full">
         <div className="w-full text-left">
-          <p className="text-2xl uppercase mx-auto max-w-6xl w-full">Explore</p>
+          <p className="text-2xl uppercase mx-auto max-w-6xl w-full">
+            {asPath ? "Posts by this author" : "Explore"}
+          </p>
           <div className="flex flex-col justify-center items-start space-y-4 mt-6">
             {posts?.map((post) => (
               <ArticleItem key={post.id} post={post} />
