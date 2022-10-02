dcf19988e18c419af8e3481d934ba405750921eb
ProCode2 <iamprobablycoding@gmail.com>
2022-10-03 03:05:32 +0530

    fix: build errors fixed

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ArticleItem from "./ArticleItem";
 
 const ArticleList = ({ posts }) => {
+  let profilePage = false;
+  if (window != undefined) {
+    profilePage = window?.location.href.includes("/profile");
+  }
   return (
     <div className="w-full h-full p-4 flex justify-start items-center mx-auto">
       <div className="max-w-6xl w-full">
         <div className="w-full text-left">
           <p className="text-2xl uppercase mx-auto max-w-6xl w-full">
-            {window?.location.href.includes("/profile")
-              ? "Posts by this author"
-              : "Explore"}
+            {profilePage ? "Posts by this author" : "Explore"}
           </p>
           <div className="flex flex-col justify-center items-start space-y-4 mt-6">
             {posts?.map((post) => (
